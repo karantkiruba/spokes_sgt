55e4905f1ef5835eaa568ed844dc94f9b10e26d3
karantkiruba <karantkiruba@hotmail.com>
2026-06-26 06:27:48 +0000

    commit on 26062026

---
+++ spokes_sgt/spokes_sgt/doctype/naming_series_manager/naming_series_manager.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, sgt and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Naming Series Manager", {
+// 	refresh(frm) {
+
+// 	},
+// });
